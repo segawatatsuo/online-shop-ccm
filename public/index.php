2653819baf7222e8432ff30144748b84c8bc04b6
segawatatsuo <base@lookingfor.jp>
2025-05-08 22:05:10 +0900

    05-08 22:05

--- public/index.php
+++ public/index.php
@@ -32,7 +32,7 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 */
 
 //require __DIR__.'/../vendor/autoload.php';
-require __DIR__.'/../../../online-shop-ccm/vendor/autoload.php';
+require __DIR__ . '/../../../online-shop-ccm/vendor/autoload.php';
 /*
 |--------------------------------------------------------------------------
 | Run The Application
@@ -45,8 +45,7 @@ require __DIR__.'/../../../online-shop-ccm/vendor/autoload.php';
 */
 
 //$app = require_once __DIR__.'/../bootstrap/app.php';
-$app = require_once __DIR__.'/../../../online-shop-ccm/bootstrap/app.php';
-
+$app = require_once __DIR__ . '/../../../online-shop-ccm/bootstrap/app.php';
 $kernel = $app->make(Kernel::class);
 
 $response = $kernel->handle(
